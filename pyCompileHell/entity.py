c10c17069f5186e1c4409a5083c00c1e571d672b
Parente Davi <parentedavi55@gmail.com>
2025-10-11 12:08:57 -0300

    Delete pyCompileHell directory

--- pyCompileHell/entity.py
+++
@@ -1,9 +0,0 @@
-class Entity:
-    def __init__(self, name):
-        self.name = name
-        self.x = 0
-        self.y = 0
-
-    def draw(self):
-        # in a real engine you'd call the native draw
-        print(f"Entity {self.name} draw at ({self.x},{self.y})")
--- pyCompileHell/scene_loader.py
+++
@@ -1,7 +0,0 @@
-import json, os
-
-def load_scene(path):
-    # minimal loader: treat .resxx as JSON for template purposes
-    with open(path, 'r') as f:
-        data = json.load(f)
-    return data
